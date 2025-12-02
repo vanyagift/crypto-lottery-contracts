d330b4a2a8c876ca4a2bd288ac9ca22fe7bbc0a6
vanyagift <ivanrubtsov4103@gmail.com>
2025-12-02 13:50:17 +0300

    fix: обновлён контракт

--- contracts/LotteryTicket.sol
+++ contracts/LotteryTicket.sol
@@ -1,51 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "erc721a/contracts/ERC721A.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract LotteryTicket is ERC721A, Ownable {
+contract LotteryTickets is ERC1155, Ownable {
     using Strings for uint256;
 
-    IERC20 public immutable lottoToken;
-    string public baseTokenURI;
+    string public baseURI;
+    mapping(uint256 => bool) public ticketExists;
 
-    constructor(address _lottoToken, string memory _baseTokenURI)
-        ERC721A("CryptoLotteryTicket", "CLT")
-        Ownable(msg.sender)
-    {
-        require(_lottoToken != address(0), "Invalid LOTTO token address");
-        lottoToken = IERC20(_lottoToken);
-        baseTokenURI = _baseTokenURI;
+    constructor(string memory _baseURI) ERC1155("") Ownable(msg.sender) {
+        baseURI = _baseURI;
     }
 
-    // Переопределяем tokenURI для поддержки метаданных
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
-        require(_exists(tokenId), "URI query for nonexistent token");
-        // Генерируем URI: ipfs://<baseTokenURI>/common/12.json или /event/12.json и т.д.
-        // Предполагаем, что мы определили редкость билета (например, через mapping)
-        // В данном примере используем простую логику: если tokenId < 90, то Common; если < 99, то Event; иначе Legendary
-        if (tokenId < 90) {
-            return string(abi.encodePacked(baseTokenURI, "/common/", Strings.toString(tokenId), ".json"));
-        } else if (tokenId < 99) {
-            return string(abi.encodePacked(baseTokenURI, "/event/", Strings.toString(tokenId), ".json"));
-        } else {
-            return string(abi.encodePacked(baseTokenURI, "/legendary/", Strings.toString(tokenId), ".json"));
+    // Только владелец может минтить
+    function mintBatch(
+        address to,
+        uint256[] calldata tokenIds,
+        uint256[] calldata amounts
+    ) external onlyOwner {
+        uint256 len = tokenIds.length;
+        require(len == amounts.length, "Arrays length mismatch");
+        for (uint256 i = 0; i < len; i++) {
+            require(amounts[i] == 1, "Only 1 ticket per ID");
+            require(!ticketExists[tokenIds[i]], "Ticket already exists");
+            ticketExists[tokenIds[i]] = true;
+            _mint(to, tokenIds[i], 1, "");
         }
     }
 
-    // Минт на указанный адрес (будет вызываться из LotteryCore)
-    function mintBatch(address to, uint256 amount) external onlyOwner {
-        require(amount > 0, "Amount must be greater than 0");
-        _mint(to, amount);
-        // Логика пула (availableTickets) теперь в LotteryCore
+    // Обновление URI (для смены домена или хостинга)
+    function setURI(string memory newURI) external onlyOwner {
+        baseURI = newURI;
+        _setURI(newURI);
+    }
+
+    // Динамический URI: https://.../api/metadata/123
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return bytes(baseURI).length > 0
+            ? string(abi.encodePacked(baseURI, tokenId.toString()))
+            : "";
     }
 }
